96aae5e42639f82b39219b99ac30640aa9141dfc
Christopher David Heanan <chrish@wesayhowhigh.com>
2024-10-27 17:27:30 +0000

    Tweaked dot styling

--- src/components/Dot.vue
+++ src/components/Dot.vue
@@ -1,28 +1,23 @@
 <script setup lang="ts">
 interface Props {
-  readonly isWhite: boolean;
-  readonly row: number;
-  readonly column: number;
+    readonly isWhite: boolean;
+    readonly row: number;
+    readonly column: number;
 }
 
 defineProps<Props>();
 
 defineEmits<{
-  flip: [];
-  flipPointer: [PointerEvent];
+    flip: [];
+    flipPointer: [PointerEvent];
 }>();
 </script>
 
 <template>
-  <button
-    type="button"
-    :data-row="row"
-    :data-column="column"
-    @pointermove="$emit('flipPointer', $event)"
-    @click="$emit('flip')"
-    class="aspect-square rounded-full transition-colors select-none m-0.5 sm:m-1"
-    :class="{ 'bg-white': isWhite, 'bg-black': !isWhite }"
-    :aria-label="isWhite ? 'white' : 'black'">
-    <div class="sr-only">{{ isWhite ? 'White' : 'Black' }}</div>
-  </button>
+    <button type="button" :data-row="row" :data-column="column" @pointermove="$emit('flipPointer', $event)"
+        @click="$emit('flip')"
+        class="aspect-square rounded-full transition-colors select-none bg-clip-content p-0.5 sm:p-1"
+        :class="{ 'bg-white': isWhite, 'bg-black': !isWhite }" :aria-label="isWhite ? 'white' : 'black'">
+        <div class="sr-only">{{ isWhite ? 'White' : 'Black' }}</div>
+    </button>
 </template>
--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -10,44 +10,40 @@ const store = usePixelStore();
 const isTouchDown = ref<boolean>(false);
 
 function touchDown(): void {
-  isTouchDown.value = true;
+    isTouchDown.value = true;
 }
 
 function touchUp(): void {
-  isTouchDown.value = false;
+    isTouchDown.value = false;
 }
 
 function flip(rowIndex: number, columnIndex: number): void {
-  store.colourIn(rowIndex, columnIndex);
+    store.colourIn(rowIndex, columnIndex);
 }
 
 function flipPointer($event: PointerEvent): void {
-  const el = document.elementFromPoint($event.clientX, $event.clientY) as HTMLElement;
+    const el = document.elementFromPoint($event.clientX, $event.clientY) as HTMLElement;
 
-  if (el.dataset.row && el.dataset.column) {
-    isTouchDown.value ? store.colourIn(parseInt(el.dataset.row), parseInt(el.dataset.column)) : null;
-  }
+    console.log(el);
+    if (el && el.dataset.row && el.dataset.column) {
+        isTouchDown.value ? store.colourIn(parseInt(el.dataset.row), parseInt(el.dataset.column)) : null;
+    }
 }
 </script>
 
 <template>
-  <div class="landscape:basis-[calc(100dvh-3rem)] portrait:max-h-[100dvw] portrait:grow flex justify-center items-center touch-none">
-    <div class="aspect-square landscape:w-[stretch] landscape:max-w-[calc(100dvh-3rem)] portrait:h-[stretch] portrait:max-h-[100dvw]">
-      <div class="px-1 grid grid-cols-[repeat(28,minmax(0,1fr))] grid-rows-[repeat(28,minmax(0,1fr))] landscape:px-2 landscape:pb-4">
-        <template v-for="(row, rowIndex) in store.pixelDots" :key="rowIndex">
-          <Dot
-            v-for="(dot, columnIndex) in row"
-            :key="columnIndex"
-            :isWhite="!!dot"
-            @pointerdown="touchDown"
-            :row="rowIndex"
-            :column="columnIndex"
-            @flip-pointer="flipPointer"
-            @flip="flip(rowIndex, columnIndex)"
-            @pointerup="touchUp"
-            @pointercancel="touchUp" />
-        </template>
-      </div>
+    <div
+        class="landscape:basis-[calc(100dvh-3rem)] portrait:max-h-[100dvw] portrait:grow flex justify-center items-center touch-none">
+        <div
+            class="aspect-square landscape:w-[stretch] landscape:max-w-[calc(100dvh-3rem)] portrait:h-[stretch] portrait:max-h-[100dvw]">
+            <div
+                class="px-1 grid grid-cols-[repeat(28,minmax(0,1fr))] grid-rows-[repeat(28,minmax(0,1fr))] landscape:px-2 landscape:pb-4">
+                <template v-for="(row, rowIndex) in store.pixelDots" :key="rowIndex">
+                    <Dot v-for="(dot, columnIndex) in row" :key="columnIndex" :isWhite="!!dot" @pointerdown="touchDown"
+                        :row="rowIndex" :column="columnIndex" @flip-pointer="flipPointer"
+                        @flip="flip(rowIndex, columnIndex)" @pointerup="touchUp" @pointercancel="touchUp" />
+                </template>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
